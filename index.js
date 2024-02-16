2da1bbac407ece95b6b4a6e3896c7bec121c0762
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-02-15 18:04:36 -0600

    slide demonstrations

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Happy Thursday!")
+// let count = 5;
+
+// while (count <= 10) {
+//     console.log(count);
+//     count++;
+// }
+
+// while (true) {
+//     console.log("LET ME OUT OF HERE!");
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     // some code
+//     console.log(i);
+// }
